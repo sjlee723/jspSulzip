a10bb3a58b314fa8b6b479bd7be0f8011ae72633
sjlee723 <sjlee723@gmail.com>
2023-03-29 17:42:17 +0900

    test

--- src/com/sulzip/app/partners/dao/PartnersDAO.java
+++ src/com/sulzip/app/partners/dao/PartnersDAO.java
@@ -3,11 +3,16 @@ package com.sulzip.app.partners.dao;
 import org.apache.ibatis.session.SqlSession;
 
 import com.mybatis.config.MyBatisConfig;
+import com.sulzip.app.partners.dto.PartnersDTO;
 
 public class PartnersDAO {
-	public SqlSession sqlsession;
+	public SqlSession sqlSession;
 	
 	public PartnersDAO() {
-		sqlsession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+	
+	public void inquiry(PartnersDTO partnersDTO) {
+		sqlSession.insert("partners.inquiry", partnersDTO);
 	}
 }
