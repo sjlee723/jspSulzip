26367ace48fc2107701f06fbab33405babfb67e8
jung-so-e <kutre121@naver.com>
2023-03-31 19:50:01 +0900

    재료 페이지

---
+++ src/com/sulzip/app/product/ProductAlcoholController.java
@@ -0,0 +1,30 @@
+package com.sulzip.app.product;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sulzip.app.Execute;
+import com.sulzip.app.product.dao.ProductDAO;
+import com.sulzip.app.product.dto.ProductDTO;
+import com.sulzip.app.sulkit.dto.SulkitDTO;
+
+public class ProductAlcoholController implements Execute {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		ProductDTO productDTO = new ProductDTO();
+		ProductDAO productDAO = new ProductDAO();
+		
+		int num = Integer.parseInt(req.getParameter("num"));
+		
+		ProductDTO productDTO = productDAO.alcohol(num);
+		
+		System.out.println(productDTO);
+		
+		req.setAttribute("alcohol", productDTO);
+		req.getRequestDispatcher("/app/product/alcoholdetail.jsp").forward(req, resp);
+		
+	}
+}
