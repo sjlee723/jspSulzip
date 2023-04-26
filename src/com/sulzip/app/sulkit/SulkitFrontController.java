e2a33ae0602cab9742ca5a3e5afffc5a4019276d
sjlee723 <sjlee723@gmail.com>
2023-04-26 18:59:29 +0900

    2023-04-26 커밋 3차

--- src/com/sulzip/app/sulkit/SulkitFrontController.java
+++ src/com/sulzip/app/sulkit/SulkitFrontController.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sulzip.app.product.AlcoholDetailController;
-import com.sulzip.app.product.AlcoholListOkController;
-
 public class SulkitFrontController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
